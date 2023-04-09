b8bf5dcbc42b2c842d04560c8566773cebc532af
santiago ruiz <santiagoruiz9416@gmail.com>
2023-04-08 23:29:10 -0300

    commit from notebook

---
+++ src/components/GeneralOdoltology/GeneralOdoltology.jsx
@@ -0,0 +1,11 @@
+
+
+const GeneralOdoltology = () => {
+  return (
+    <div>
+        <h5 className="text-center text-5xl font-bold pt-20 pb-5">Odontologia General</h5>
+    </div>
+  )
+}
+
+export default GeneralOdoltology
